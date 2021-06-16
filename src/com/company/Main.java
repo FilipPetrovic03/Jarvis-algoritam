0cb4c589c6b55d7c35653c0b1e31e095e8859dfa
FilipPetrovic03 <2018.10.14@online.osmagimnazija.edu.rs>
2021-06-16 20:08:31 +0200

    Kreiranje metode orijentacija
    Kreiranje metode orijentacija

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
     static class Point {
         int x;
@@ -15,7 +17,13 @@ public class Main {
                 if (next[i] != -1) {
                     System.out.println("( " + points[i].x + ", " + points[i].y + " )");
                 }
-            }
+        }
+
+        private boolean orijentacija(Point p, Point q, Point r) {
+            int val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y);
+            return !(val >= 0);
+        }
+
 
     }
 
