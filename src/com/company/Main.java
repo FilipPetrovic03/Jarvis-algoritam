329dda57ba2b34bc3f7c2af2a5d7b7349abfd08d
FilipPetrovic03 <2018.10.14@online.osmagimnazija.edu.rs>
2021-06-16 20:05:22 +0200

    Kreiranje klase Jarvis
    Kreiranje klase Jarvis i dodavanje metode za ispis koordinata.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,21 @@ public class Main {
         int x;
         int y;
     }
+
+    public class Jarvis {
+
+        public void ispisi(Point[] points, int[] next) {
+            System.out.println("\ntacke : ");
+
+            for (int i = 0; i < next.length; i++)
+                if (next[i] != -1) {
+                    System.out.println("( " + points[i].x + ", " + points[i].y + " )");
+                }
+            }
+
+    }
+
+
     public static void main(String[] args) {
 
     }
